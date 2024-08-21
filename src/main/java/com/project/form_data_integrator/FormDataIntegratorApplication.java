fe6552b617537a8c09088cef22c3b17674d9b7a8
Leonardo <leorod2005@gmail.com>
2024-08-21 17:55:55 -0300

    initial commit

---
+++ src/main/java/com/project/form_data_integrator/FormDataIntegratorApplication.java
@@ -0,0 +1,13 @@
+package com.project.form_data_integrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormDataIntegratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormDataIntegratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/form_data_integrator/FormDataIntegratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.form_data_integrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormDataIntegratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
