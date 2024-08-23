c45f6cb66410646951fe3ad2fc79703392f90c4a
Leonardo <leorod2005@gmail.com>
2024-08-22 23:47:27 -0300

    fix: confirm password validation not working

--- src/main/java/com/project/form_data_integrator/controllers/RegisterController.java
+++ src/main/java/com/project/form_data_integrator/controllers/RegisterController.java
@@ -3,27 +3,39 @@ package com.project.form_data_integrator.controllers;
 import com.project.form_data_integrator.dto.RegistrationDTO;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("register")
 public class RegisterController {
 
-    @GetMapping
+    @GetMapping("/success")
+    public String successView(Model model){
+        return "success";
+    }
+
+
+    @GetMapping("/register")
     public ModelAndView registerView(RegistrationDTO registrationDTO){
         return new ModelAndView("register");
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public ModelAndView register(@Valid RegistrationDTO registrationDTO, BindingResult bindingResult){
+        if(!registrationDTO.password().equals(registrationDTO.confirmPassword())){
+            ModelAndView mv = new ModelAndView("register");
+            mv.addObject("passwordError", true);
+            return mv;
+        }
+
         if(bindingResult.hasErrors()){
             return new ModelAndView("register");
         }
 
-        return new ModelAndView("register");
+        System.out.println(registrationDTO);
+        return new ModelAndView("redirect:/success");
     }
 }
